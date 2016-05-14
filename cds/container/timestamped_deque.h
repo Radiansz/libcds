6b222d10da519200dcd51240fbb71799d473fe43
Radiansz <lightwaveinthespace@gmail.com>
2016-05-14 07:25:10 +0300

    Fixed 1 error and few warnings

--- cds/container/timestamped_deque.h
+++ cds/container/timestamped_deque.h
@@ -168,7 +168,7 @@ namespace cds { namespace container {
 			 startCandidate.clear();
 			 startPoint.clear();
 			 toRemove.clear();
-			 bnode *b = candidate.get<bnode>();
+
 			 bool empty = true, isFound = false;
 			 success = true;
 			 int threadIND = acquireIndex();
@@ -594,7 +594,7 @@ namespace cds { namespace container {
 						bool temp = false;
 
 						garbage_node* gNode = new garbage_node(node);
-						garbage_node* tmp;
+
 						buffer_node* cur = node,
 								*next = node;
 						bool fromLeft = cur->isDeletedFromLeft;
@@ -774,9 +774,9 @@ namespace cds { namespace container {
 
 					bool tryToSetBorder(buffer_node* newOne, buffer_node* oldOne, bool leftest) {
 						if(leftest)
-							leftMost.compare_exchange_strong(oldOne, newOne);
+							return leftMost.compare_exchange_strong(oldOne, newOne);
 						else
-							rightMost.compare_exchange_strong(oldOne, newOne);
+							return rightMost.compare_exchange_strong(oldOne, newOne);
 					}
 
 					void insert(node* timestamped, bool toLeft) {
@@ -983,9 +983,9 @@ namespace cds { namespace container {
 					}
 
 					void write(std::string s) {
-						int index = acquireIndex();
-						LogNode* node = new LogNode(s, platform::getTimestamp(), index);
-						threads.at(index)->push_back(node);
+//						int index = acquireIndex();
+//						LogNode* node = new LogNode(s, platform::getTimestamp(), index);
+//						threads.at(index)->push_back(node);
 					}
 
 					void printAll() {
