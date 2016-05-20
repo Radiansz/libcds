4d3e5896236c4cacf258b08e8ded30c7957e9bf0
Radiansz <lightwaveinthespace@gmail.com>
2016-05-21 00:37:47 +0300

    More output params

--- collect.sh
+++ collect.sh
@@ -1,14 +1,14 @@
 COUNTER=25
 STEPS=20
 THR=2
-
+echo executionTime,operationWithEmpty,writesAmountPerThread,readsAmountPerThread,writers,readers,emptyOperations,failedOperations,freedNodes
 while [ "$STEPS" -ne  0 ]
 do
 	echo $STEPS
 	COUNTER=3
 	while [ "$COUNTER" -ne 0 ]
 	do
-		bin/tsdeque_stress -out $HOME/result.txt -wa 4000000 -ra 4000000 -wt $THR -rt $THR --gtest_filter=*queue*
+		bin/tsdeque_stress -out $HOME/result.txt -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*queue*
 		COUNTER=$(( $COUNTER - 1 ))
 	done
 	THR=$(( $THR + 1 ))
