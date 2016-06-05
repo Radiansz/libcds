0a992e0e16022bba3212910c12884471253d02e8
Radiansz <lightwaveinthespace@gmail.com>
2016-06-05 04:07:08 +0300

    Modified script

--- collect.sh
+++ collect.sh
@@ -1,9 +1,11 @@
-COUNTER=3
-STEPS=4
+GLOBSTEPS=3
+STEPS=$GLOBSTEPS
 THR=2
-FILE=$HOME/result.txt
+ROOT=$(pwd)
+FILE=$ROOT/result.txt
 echo executionTime,operationWithEmpty,writesAmountPerThread,readsAmountPerThread,writers,readers,emptyOperations,failedOperations,freedNodes >> $FILE
 echo queue >> $FILE
+# queue
 while [ "$STEPS" -ne  0 ]
 do
 	echo $STEPS
@@ -16,9 +18,8 @@ do
 	THR=$(( $THR + 1 ))
 	STEPS=$(( $STEPS - 1 ))
 done
-
-COUNTER=3
-STEPS=4
+# stack
+STEPS=$GLOBSTEPS
 THR=2
 echo stack >> $FILE
 while [ "$STEPS" -ne  0 ]
@@ -27,15 +28,14 @@ do
 	COUNTER=5
 	while [ "$COUNTER" -ne 0 ]
 	do
-		bin/tsdeque_stress -out $HOME/result.txt -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*stack*
+		bin/tsdeque_stress -out $FILE  -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*stack*
 		COUNTER=$(( $COUNTER - 1 ))
 	done
 	THR=$(( $THR + 1 ))
 	STEPS=$(( $STEPS - 1 ))
 done
-
-COUNTER=3
-STEPS=4
+# deque
+STEPS=$GLOBSTEPS
 THR=2
 echo deque >> $FILE
 while [ "$STEPS" -ne  0 ]
@@ -44,9 +44,108 @@ do
 	COUNTER=5
 	while [ "$COUNTER" -ne 0 ]
 	do
-		bin/tsdeque_stress -out $HOME/result.txt -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*deque*
+		bin/tsdeque_stress -out $FILE  -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*deque*
 		COUNTER=$(( $COUNTER - 1 ))
 	done
 	THR=$(( $THR + 1 ))
 	STEPS=$(( $STEPS - 1 ))
 done
+# queue consumer
+echo queue_consumer >> $FILE
+STEPS=3
+THR=2
+while [ "$STEPS" -ne  0 ]
+do
+	echo $STEPS
+	COUNTER=5
+	while [ "$COUNTER" -ne 0 ]
+	do
+		bin/tsdeque_stress_consumer -out $FILE -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*queue*
+		COUNTER=$(( $COUNTER - 1 ))
+	done
+	THR=$(( $THR + 1 ))
+	STEPS=$(( $STEPS - 1 ))
+done
+# stack consumer
+STEPS=$GLOBSTEPS
+THR=2
+echo stack_consumer >> $FILE
+while [ "$STEPS" -ne  0 ]
+do
+	echo $STEPS
+	COUNTER=5
+	while [ "$COUNTER" -ne 0 ]
+	do
+		bin/tsdeque_stress_consumer -out $FILE  -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*stack*
+		COUNTER=$(( $COUNTER - 1 ))
+	done
+	THR=$(( $THR + 1 ))
+	STEPS=$(( $STEPS - 1 ))
+done
+# deque consumer
+STEPS=$GLOBSTEPS
+THR=2
+echo deque_producer >> $FILE
+while [ "$STEPS" -ne  0 ]
+do
+	echo $STEPS
+	COUNTER=5
+	while [ "$COUNTER" -ne 0 ]
+	do
+		bin/tsdeque_stress_producer -out $FILE  -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*deque*
+		COUNTER=$(( $COUNTER - 1 ))
+	done
+	THR=$(( $THR + 1 ))
+	STEPS=$(( $STEPS - 1 ))
+done
+# queue producer
+echo queue_producer >> $FILE
+STEPS=$GLOBSTEPS
+THR=2
+while [ "$STEPS" -ne  0 ]
+do
+	echo $STEPS
+	COUNTER=5
+	while [ "$COUNTER" -ne 0 ]
+	do
+		bin/tsdeque_stress_producer -out $FILE -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*queue*
+		COUNTER=$(( $COUNTER - 1 ))
+	done
+	THR=$(( $THR + 1 ))
+	STEPS=$(( $STEPS - 1 ))
+done
+# stack producer
+STEPS=$GLOBSTEPS
+THR=2
+echo stack_producer >> $FILE
+while [ "$STEPS" -ne  0 ]
+do
+	echo $STEPS
+	COUNTER=5
+	while [ "$COUNTER" -ne 0 ]
+	do
+		bin/tsdeque_stress_producer -out $FILE  -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*stack*
+		COUNTER=$(( $COUNTER - 1 ))
+	done
+	THR=$(( $THR + 1 ))
+	STEPS=$(( $STEPS - 1 ))
+done
+# deque producer
+STEPS=$GLOBSTEPS
+THR=2
+echo deque_producer >> $FILE
+while [ "$STEPS" -ne  0 ]
+do
+	echo $STEPS
+	COUNTER=5
+	while [ "$COUNTER" -ne 0 ]
+	do
+		bin/tsdeque_stress_producer -out $FILE  -wa 1000000 -ra 1000000 -wt $THR -rt $THR --gtest_filter=*deque*
+		COUNTER=$(( $COUNTER - 1 ))
+	done
+	THR=$(( $THR + 1 ))
+	STEPS=$(( $STEPS - 1 ))
+done
+
+
+
